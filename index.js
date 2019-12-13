a132e005436c13bf46b5b0d7eeed75165bc72e8f
Andrew Baker <andrew_j_baker2@hotmail.com>
2019-12-13 20:14:35 +0000

    Day 1, part 2

--- index.js
+++ index.js
@@ -1,11 +1,26 @@
 const fs = require('fs')
 
+const calculate = mass => Math.floor(mass / 3) - 2
+
+function calculateFuelRequirement (cumulativeSumOfAdditionalFuel, mass) {
+  const adjustedMass = calculate(mass)
+
+  return adjustedMass > 0
+    ? calculateFuelRequirement(cumulativeSumOfAdditionalFuel + adjustedMass,
+      adjustedMass)
+    : cumulativeSumOfAdditionalFuel
+}
+
 const moduleMasses = fs.readFileSync('puzzle-input.dat')
   .toString()
   .split('\n')
-  .slice(0, -1)
+  .slice(0, -1) // remove last element (NaN)
 
-const sumOfFuelRequirements = moduleMasses.map(mass => Math.floor(mass / 3) - 2)
+const sumOfFuelRequirements = moduleMasses.map(mass => {
+  const initialMass = calculate(mass)
+
+  return calculateFuelRequirement(initialMass, initialMass)
+})
   .reduce((cumulativeSumOfFuelRequirements, fuelRequirement) =>
     cumulativeSumOfFuelRequirements + fuelRequirement)
 
